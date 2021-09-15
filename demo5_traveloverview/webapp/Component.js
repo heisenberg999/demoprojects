5c7f633bf5cffd473053de9f51d9e944e00e91e4
Byom <byomrocks@gmail.com>
2021-09-15 15:48:53 +0530

    Travel OVP App

---
+++ demo5_traveloverview/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/ovp/app/Component'], function(AppComponent) {
+    return AppComponent.extend("sap.fe.demo.demo5traveloverview.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
